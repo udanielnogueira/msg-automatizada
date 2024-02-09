902626c21e09031799642438633a4a9a815496d8
unknown <danielsilvangr@gmail.com>
2024-02-09 15:34:46 -0300

    Atualizando os arquivos

--- script.py
+++ script.py
@@ -2,9 +2,9 @@ import pyautogui as pg
 import random
 import time
 
-msg = ['Bombarda', 'Avada Kedavra', 'Priori Incantatem','Expecto Patronum']
+msg = ['Bombarda', 'Avada Kedavra', 'Priori Incantatem', 'Expecto Patronum']
 
-# tempo de espera para o disparo
+# Tempo de espera para o disparo
 time.sleep(8)
 
 for i in range (10):
